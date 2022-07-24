eac82f386eaacf45eb6cc337a11e78a24b77972c
vtlkr <vtlkr@rocke.tech>
2022-07-24 07:56:14 +0300

    Initial commit

---
+++ src/main/java/com/example/genesispractice/GenesisPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.genesispractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesisPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenesisPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/genesispractice/GenesisPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genesispractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesisPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
